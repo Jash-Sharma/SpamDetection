b12f773366efde3753b746a15898eda01ec3e468
cruelcaretaker <jashsharma1310@gmail.com>
2024-04-17 03:26:18 +0530

    updating the classifier

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ nltk.download('stopwords')
 
 ps = PorterStemmer()
 
-
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-
     y = []
     for i in text:
         if i.isalnum():
@@ -38,12 +36,23 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
+phishing = ["verify","account","suspend","login","secure","update","identity",'reset',"password",'confirm','security',"alert","authorize","verification","details"]
+promo = ['sale','offer','special','exclusive','limited','discount','price','code','buy','free','promotional','clearance','access','only','last','bonus','one','savings','alert','event']
+scams = ['action','bonus','building','cash','claim','confirmed','congratulations','draw','earnings','easy','exclusive','fast','final','financial','free','freedom','get','gift','guaranteed','instant','instructions','investment','jackpot','lottery','lucky','millionaire','money','needed','notice','notification','now','offer','opportunity','paid','payout','prize','profit','quick','required','response','reward','rich','risk-free','secret','secure','selected','strategy','transfer','unclaimed','urgent','waiting','wealth','win','windfall','winner','winning','winnings','won',"you've",'your']   
+malware = ['access','action','activity','advisory','against','alert','antivirus','assistance','at','attack','attempt','breach','compromised','critical','cyber','cybersecurity','dangerous','data','detected','device','encryption','file','files','firewall','hacking','harmful','identity','immediate','infected','infection','link','malicious','malware','notice','online','patch','phishing','protect','protection','ransomware','removal','required','risk','scam','scan','secure','security','software','spyware','system','theft','threat','unauthorized','update','urgent','virus','vulnerability','warning','your']
+adult = ['adult','cam','chat','club','content','dating','entertainment','erotic','explicit','film','fun','games','girls','hookup','hot','live','mature','meet','naughty','nude','only','personals','pics','play','private','scene','scenes','service','sex','sexy','show','singles','stars','uncensored','video','videos','website','xxx','zone']
+
+catergory = {'promo' : 0,
+             'adult' : 0,
+             'scams' : 0,
+             'phishing': 0,
+             'malware' : 0}
+
 st.title("SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
@@ -54,6 +63,23 @@ if st.button('Predict'):
     st.title("Result :")
     if result == 1:
         st.header("It is a spam")
+        for i in input_sms.lower().split(" "):  
+            if i in adult:
+                catergory['adult'] += 1
+            elif i in malware:
+                catergory['malware'] += 1
+            elif i in promo:
+                catergory['promo'] += 1
+            elif i in phishing:
+                catergory['phishing'] += 1  
+            elif i in scams:
+                catergory['scams'] += 1
+        maxi = 0
+        for i in catergory:
+            if catergory[i] > maxi:
+                maxi = catergory[i]
+                spam = i
+        st.header(f'It is a/an {spam} spam')
     else:
         st.header("It is not a spam")
 
@@ -81,7 +107,7 @@ text-align: center;
 }
 </style>
 <div class="footer">
-<p>Developed with ❤ by <a style='text-align: center;' href="https://jash-sharma.github.io/Portfolio/" target="_blank">Jash Sharma</a></p>
+<p>Developed with ❤ by Jash, Sakshi and Savithri</p>
 <p>Might Make Mistakes</p>
 </div>
 """
